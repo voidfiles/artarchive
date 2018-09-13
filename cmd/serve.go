84f71a8acd21c0fa43dded4d2440c7cefdbe5336
Alex Kessinger <voidfiles@gmail.com>
2018-09-13 17:55:33 -0600

    Awesome

---
+++ cmd/serve.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/spf13/cobra"
+)
+
+func serve() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "")
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.Run(":" + port)
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+}
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Runs an http server",
+	Long:  `Runs an http server`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Print("cmd: serve")
+		serve()
+	},
+}
